c12f6157e3ded4e2340b4ab6b7fe161b16f3b87e
07JP27 <jynpei.t@gmail.com>
2021-09-12 12:08:18 +0900

    Add Color Bulb command follow original.

---
+++ DoinJomain.Switchbot/Requests/ColorBulb.cs
@@ -0,0 +1,87 @@
+﻿using DoinJomain.Switchbot.Constants;
+using DoinJomain.Switchbot.Enums;
+using DoinJomain.Switchbot.Exceptions;
+using DoinJomain.Switchbot.Models;
+using DoinJomain.Switchbot.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoinJomain.Switchbot
+{
+    public class ColorBulb: BaseDevice
+    {
+        public ColorBulb(SwitchbotClient client): base(client)
+        {
+        }
+
+        public Task<CommandExecuteResoponse> TurnOnAsync(string deviceId)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.TurnOn,
+                Parameter = CommandParameter.Default
+            };
+
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+
+        public Task<CommandExecuteResoponse> TurnOffAsync(string deviceId)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.TurnOff,
+                Parameter = CommandParameter.Default
+            };
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+
+        public Task<CommandExecuteResoponse> ToggleAsync(string deviceId)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.Toggle,
+                Parameter = CommandParameter.Default
+            };
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+
+        public Task<CommandExecuteResoponse> SetBrightnessAsync(string deviceId, int brightness)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.SetBrightness,
+                Parameter = brightness.ToString()
+            };
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+
+        public Task<CommandExecuteResoponse> SetColorAsync(string deviceId, RgbColor color)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.SetColor,
+                Parameter = $"{color.Red}:{color.Green}:{color.Blue}"
+            };
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+
+        public Task<CommandExecuteResoponse> SetColorTemperature(string deviceId, int temperature)
+        {
+            var parameters = new CommandRequestBody()
+            {
+                CommandType = CommandType.Commnad,
+                Command = Command.SetColorTemperature,
+                Parameter = temperature.ToString()
+            };
+            return this.CommandExecuteAsync(deviceId, parameters);
+        }
+    }
+}
---
+++ DoinJomain.Switchbot/Structs/RgbColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoinJomain.Switchbot.Structs
+{
+    public struct RgbColor
+    {
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+
+        public RgbColor(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+}
